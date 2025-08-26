a126782a4f5da7d5dceca303bb34892b39610e4f
Karl-codeing <karlmagzucker@gmail.com>
2025-08-26 22:54:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ async function sendMsg() {
   chat.scrollTop = chat.scrollHeight;
 
   try {
-    let res = await fetch("http://192.168.56.1:5000/ask", {
+    let res = await fetch("http://192.168.2.127:5000/ask", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ prompt: msg })
